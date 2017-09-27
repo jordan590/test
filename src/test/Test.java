4b50f2a7384fe8023e91a66325f3fd292555b7d4
jordan590 <32143262+jordan590@users.noreply.github.com>
2017-09-27 14:41:30 -0400

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,6 +17,7 @@ public class Test {
      */
     public static void main(String[] args) {
         System.out.println("Will it upload?");
+        System.out.println("Now, will it download?");
     }
     
 }
